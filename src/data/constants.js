7aa593219ad1b56dc86b05478efd757d21440544
JEFFYFINNY <jeffyfinny@gmail.com>
2024-11-01 12:43:06 +0530

    made some changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/JEFFYFINNY",
     resume:
-      "https://drive.google.com/file/d/1JHf1su4UaH2grDOmAD3LO9oDwwJcICCE/view?usp=drive_link",
+      "https://drive.google.com/file/d/1crdwRdA_9WGiyi_LRfX6uyiqhFC0_iDf/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/jeffy-finny-536698257/",
     insta: "https://www.instagram.com/_j_e_f_f_y_/",
   };
